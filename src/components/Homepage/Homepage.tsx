21a21b5df15ea986778cb7f4768bcf5ffdfc529b
Rahul <raguly55@gmail.com>
2023-01-21 17:54:20 +0530

    configured webpack for build tym

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 // import styles from './Homepage.module.scss';
+import Logo from './logo192.png';
 
 const Homepage = () => {
 
     return <>
         <h1 >Home page under constraction</h1>
+        <img src={Logo} alt="nothing.." />
     </>
 }
 
